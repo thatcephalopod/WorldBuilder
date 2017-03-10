d65e6327643d1e5af3e4e8b6c747b633d229d697
thatcephalopod <thatcephalopod.sam@gmail.com>
2017-03-09 20:00:53 -0500

    Fixed the constructor for loot
    The loot constructor now adds all of the items within the given table to
    the proper ArrayList

--- src/worldbuilder/Loot.java
+++ src/worldbuilder/Loot.java
@@ -6,46 +6,51 @@ import java.util.ArrayList;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-
 /**
- * The loot class in intended for generating randomized loot drops based off of a given list of items and quantities given in an input file.
+ * The loot class in intended for generating randomized loot drops based off of
+ * a given list of items and quantities given in an input file.
+ *
  * @author Sam
  */
 public class Loot {
-    
+
     String address;
     ArrayList<ItemOption> possibilities = new ArrayList<ItemOption>();
-    
-    public Loot(String a) throws FileNotFoundException, IOException {
+
+    public Loot(String a) throws IOException {
         FileReader read;
         Scanner ui;
-        
+
         try {
             read = new FileReader(a);
             ui = new Scanner(read);
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             throw e;
         }
-        
-        //Reading the file
-        while(ui.hasNextLine()) {
-            //This is a fairly fragile way of reading the file.
-            //If the file ever breaks the syntax, the whole program fails.
-            String name = ui.next();
-            String quality = ui.next();
-            if(quality == "~")
-                quality = "";
-            int qual = ui.nextInt();
-            int min = ui.nextInt();
-            int max = ui.nextInt();
-            int g = ui.nextInt();
-            
-            Item temp = new Item(name, quality, qual, g);
-            possibilities.add(new ItemOption(temp, min, max));
-                       
+
+        try {
+            //Reading the file
+            while (ui.hasNextLine()) {
+                //This is a fairly fragile way of reading the file.
+                //If the file ever breaks the syntax, the whole program fails.
+                String name = ui.next();
+                String quality = ui.next();
+                if (quality.equals("~")) {
+                    quality = "";
+                }
+                int qual = ui.nextInt();
+                int min = ui.nextInt();
+                int max = ui.nextInt();
+                int g = ui.nextInt();
+             
+                Item temp = new Item(name, quality, qual, g);
+                possibilities.add(new ItemOption(temp, min, max));
+
+            }
+        } catch (Exception e) {
+            throw e;
         }
-        
-        
+
     }
-    
+
 }
